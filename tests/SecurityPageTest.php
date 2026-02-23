49f4e87e20da4e08eb6a6ee6c3f0bac5354832ce
Wil <wilbou.adrar@gmail.com>
2026-02-23 15:51:46 +0100

    test

--- tests/SecurityPageTest.php
+++ tests/SecurityPageTest.php
@@ -29,84 +29,93 @@ class SecurityPageTest extends WebTestCase
         $this->assertSelectorTextContains('h1', 'Connexion');
     }
 
-    // public function testRegistrationWorks(): void
+    // TEST CONNEXION CANDIDAT
+    // public function testLoginSuccessAsCandidate(): void
     // {
     //     $client = static::createClient();
-    //     $crawler = $client->request('GET', '/register');
+    //     $crawler = $client->request('GET', '/');
 
-    //     // On sélectionne le bouton "S'inscrire" (par son texte)
-    //     $buttonCrawlerNode = $crawler->selectButton('S\'inscrire');
 
-    //     // On récupère le formulaire lié à ce bouton
-    //     $form = $buttonCrawlerNode->form([
-    //         'registration_form[email]' => 'test-unitaire@test.com',
-    //         'registration_form[plainPassword]' => 'CorrectPassword123456',
-    //         'registration_form[user_type]' => 'candidate',
-    //         'registration_form[agreeTerms]' => true,
-    //     ]);
+    // }
 
-    //     // On soumet le formulaire
-    //     $client->submit($form);
+    public function testRegistrationWorks(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/register');
 
-    //     // On vérifie qu'on est redirigé (souvent vers la home après inscription)
-    //     $this->assertResponseRedirects('/home');
+        // On sélectionne le bouton "S'inscrire" (par son texte)
+        $buttonCrawlerNode = $crawler->selectButton('S\'inscrire');
+
+        // On récupère le formulaire lié à ce bouton
+        $form = $buttonCrawlerNode->form([
+            'registration_form[email]' => 'test-unitaire@test.com',
+            'registration_form[plainPassword]' => 'CorrectPassword123456',
+            'registration_form[user_type]' => 'candidate',
+            'registration_form[agreeTerms]' => true,
+        ]);
+
+        // On soumet le formulaire
+        $client->submit($form);
+
+        // On vérifie qu'on est redirigé (souvent vers la home après inscription)
+        $this->assertResponseRedirects('/home');
         
-    //     // On suit la redirection pour vérifier le message de succès si tu en as un
-    //     $client->followRedirect();
-    // }
+        // On suit la redirection pour vérifier le message de succès si tu en as un
+        $client->followRedirect();
+    }
 
-    // public function testLoginWorks(): void
-    // {
-    //     $client = static::createClient();
-    //     $client->request('GET', '/');
+    public function testLoginWorks(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
 
-    //     // On remplit le formulaire avec les identifiants créés dans AddFixtures
-    //     $client->submitForm('Se connecter', [
-    //         'email'=> 'test-ci@test.com',
-    //         'password'=> 'SuperPassWord123',
-    //     ]);
+        // On remplit le formulaire avec les identifiants créés dans AddFixtures
+        $client->submitForm('Se connecter', [
+            'email'=> 'test-ci@test.com',
+            'password'=> 'SuperPassWord123',
+        ]);
 
-    //     //  On vérifie que la connexion redirige vers /home
-    //     $this->assertResponseRedirects('/home');
+        //  On vérifie que la connexion redirige vers /home
+        $this->assertResponseRedirects('/home');
 
-    //     // On suit la redirection pour vérifier la connexion
-    //     $client->followRedirect();
-    // }
+        // On suit la redirection pour vérifier la connexion
+        $client->followRedirect();
+    }
 
-    // public function testAddMission(): void
-    // {
-    //     $client = static::createClient();
-    //     $container = static::getContainer();
-    //     $entityManager = $container->get('doctrine.orm.entity_manager');
+    public function testAddMission(): void
+    {
+        $client = static::createClient();
+        $container = static::getContainer();
+        $entityManager = $container->get('doctrine.orm.entity_manager');
 
-    //     // --- AJOUT POUR LA CONNEXION ---
-    //     $userRepository = $container->get(UserRepository::class);
-    //     $testUser = $userRepository->findOneBy(['email' => 'test-ci@test.com']);
-    //     $client->loginUser($testUser);
+        // --- AJOUT POUR LA CONNEXION ---
+        $userRepository = $container->get(UserRepository::class);
+        $testUser = $userRepository->findOneBy(['email' => 'test-ci@test.com']);
+        $client->loginUser($testUser);
 
-    //     //Je récupère les données pour skill et wageScale
-    //     $wage = $container->get(\App\Repository\WageScaleRepository::class)->findOneBy([]);
-    //     $skill = $container->get(\App\Repository\SkillsRepository::class)->findOneBy([]);
+        //Je récupère les données pour skill et wageScale
+        $wage = $container->get(\App\Repository\WageScaleRepository::class)->findOneBy([]);
+        $skill = $container->get(\App\Repository\SkillsRepository::class)->findOneBy([]);
         
-    //     $crawler = $client->request('GET', '/create/mission');
+        $crawler = $client->request('GET', '/create/mission');
 
-    //     $this->assertResponseIsSuccessful();
+        $this->assertResponseIsSuccessful();
 
-    //     // On sélectionne le bouton "Publier la mission" (par son texte)
-    //     $buttonCrawlerNode = $crawler->selectButton('Publier la mission');
+        // On sélectionne le bouton "Publier la mission" (par son texte)
+        $buttonCrawlerNode = $crawler->selectButton('Publier la mission');
 
-    //     // On récupère le formulaire lié à ce bouton
-    //     $form = $buttonCrawlerNode->form([
-    //         'add_mission[title]'=> 'Mission test',
-    //         'add_mission[description]'=> 'Mission test et description pour tester tout ça',
-    //         'add_mission[startAt]'=> '2027-01-22T08:00',
-    //         'add_mission[endAt]'=> '2027-01-22T12:00',
-    //         'add_mission[areaLocation]'=> '31500 - Toulouse',
-    //         'add_mission[wageScale]'=> $wage->getId(),
-    //         'add_mission[skills]'=> [$skill->getId()],
-    //     ]);
-    //     $client->submit($form);
-    //     $this->assertResponseRedirects('/home');
-    // }
+        // On récupère le formulaire lié à ce bouton
+        $form = $buttonCrawlerNode->form([
+            'add_mission[title]'=> 'Mission test',
+            'add_mission[description]'=> 'Mission test et description pour tester tout ça',
+            'add_mission[startAt]'=> '2027-01-22T08:00',
+            'add_mission[endAt]'=> '2027-01-22T12:00',
+            'add_mission[areaLocation]'=> '31500 - Toulouse',
+            'add_mission[wageScale]'=> $wage->getId(),
+            'add_mission[skills]'=> [$skill->getId()],
+        ]);
+        $client->submit($form);
+        $this->assertResponseRedirects('/home');
+    }
     
 }
